567bf1e8800f1c25f0a527d26c1643929eb6b653
wrote-code <developer@noreply.email.com>
2023-03-03 01:01:22 +0800

    函数定义和调用

---
+++ 12 函数/han_shu.py
@@ -0,0 +1,15 @@
+def hello_world():
+    print("hello,world")
+
+
+def out_string(s1, s2):
+    print(s1 + s2)
+
+
+def area(a, b):
+    return a * b
+
+
+hello_world()
+out_string(1, 2)
+print(area(10, 20))
